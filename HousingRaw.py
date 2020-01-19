7f3415e3c8bfcdc749bde8732c6e361beac2da07
Anmol Verma <anmol.verma4@gmail.com>
2020-01-19 19:50:24 +0530

    added housing initial code

---
+++ HousingRaw.py
@@ -0,0 +1,43 @@
+import os
+import tarfile
+from six.moves import urllib
+import pandas as pd
+import matplotlib.pyplot as plt
+from pandas.plotting import scatter_matrix
+
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
+HOUSING_PATH = "datasets/housing"
+HOUSING_URL = DOWNLOAD_ROOT + HOUSING_PATH + "/housing.tgz"
+
+
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    if not os.path.isdir(housing_path):
+        os.makedirs(housing_path)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+
+
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
+
+
+fetch_housing_data()
+housing = load_housing_data()
+housing.head()
+housing.info()
+
+housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.4, s=housing["population"] / 100, label="population",
+             figsize=(10, 7), c="median_house_value", cmap=plt.get_cmap("jet"), colorbar=True)
+plt.legend()
+plt.show()
+
+attributes = ["median_house_value", "median_income", "total_rooms", "housing_median_age"]
+matrixOFScatteredPlots = scatter_matrix(housing[attributes], figsize=(12, 8))
+
+housing.plot(kind="scatter", x="median_income", y="median_house_value", alpha=0.1)
+plt.legend()
+plt.show()
